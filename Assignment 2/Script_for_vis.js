81c78d0f0c3f2bf9b88cd1b2431a6a1190464cad
Cheryl Neoh <cneo0003@student.monash.edu>
2020-10-28 19:41:45 +0800

    change size

--- Assignment 2/Script_for_vis.js
+++ Assignment 2/Script_for_vis.js
@@ -1,8 +1,7 @@
 var crime_rate_vis_path = "https://Cheryl-Neoh.github.io/FIT3179/Assignment 2/Crime_rates_visualisation.json";
-vegaEmbed('#crime_rate_vis', crime_rate_vis_path).then(function(result){
+vegaEmbed('#crime_rate_vis', crime_rate_vis_path).then(function (result) {
 }).catch(console.error);
 
 var drug_addict_vis_path = "https://Cheryl-Neoh.github.io/FIT3179/Assignment 2/Drug_visualisation.json";
-vegaEmbed('#drug_addict_vis', drug_addict_vis_path).then(function(result){
+vegaEmbed('#drug_addict_vis', drug_addict_vis_path).then(function (result) {
 }).catch(console.error);
-
